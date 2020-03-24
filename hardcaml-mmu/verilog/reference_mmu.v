2b482382fae8aa09278ba0641afba2618e698190
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2020-03-23 23:46:10 -0400

    Maybe fix spaces

--- hardcaml-mmu/verilog/reference_mmu.v
+++ hardcaml-mmu/verilog/reference_mmu.v
@@ -20,15 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 module reference_mmu(
 	input [2:0] address,
-   input reset_n,
-   output eeprom_ce_n,
-   output ram_ce_n,
-	output via_ce_n,
-   output acia_ce_n
+  input       reset_n,
+  output      eeprom_ce_n,
+  output      ram_ce_n,
+  output      via_ce_n,
+  output      acia_ce_n
 	);
-	 
-	assign eeprom_ce_n = reset_n ? ~(address[2] & address[1] & address[0]) : 1;
-	assign ram_ce_n = reset_n ? address[2] : 1;
-	assign via_ce_n = reset_n ? ~(address[2] & ~address[1]) : 1;
-	assign acia_ce_n = reset_n ? ~(address[2] & address[1] & ~address[0]) : 1;
+   assign eeprom_ce_n = reset_n ? ~(address[2] & address[1] & address[0]) : 1;
+	 assign ram_ce_n = reset_n ? address[2] : 1;
+	 assign via_ce_n = reset_n ? ~(address[2] & ~address[1]) : 1;
+	 assign acia_ce_n = reset_n ? ~(address[2] & address[1] & ~address[0]) : 1;
 endmodule
