5ff999aacb0e199eb60e1df8bc36626dfad15b3d
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2020-03-28 23:30:09 -0400

    More spaces

--- bootstrap-mmu/memory_copier_test.v
+++ bootstrap-mmu/memory_copier_test.v
@@ -2,41 +2,41 @@
 
 module memory_copier_test;
 
-	 // Inputs
-	 reg reset_n;
-	 reg clock;
+   // Inputs
+   reg reset_n;
+   reg clock;
 
-	 // Outputs
-	 wire [15:0] address;
-	 wire        ram_we_n;
-	 wire        ram_cs_n;
-	 wire        eeprom_oe_n;
-	 wire        eeprom_cs_n;
-	 wire        done;
+   // Outputs
+   wire [15:0] address;
+   wire        ram_we_n;
+   wire        ram_cs_n;
+   wire        eeprom_oe_n;
+   wire        eeprom_cs_n;
+   wire        done;
 
-	 // Instantiate the Unit Under Test (UUT)
-	 memory_copier
+   // Instantiate the Unit Under Test (UUT)
+   memory_copier
      #(.EEPROM_ADDRESS_BUS_WIDTH(4))
    uut (
-		    .reset_n(reset_n),
-		    .clock(clock),
-		    .address(address),
-		    .ram_we_n(ram_we_n),
-		    .ram_cs_n(ram_cs_n),
-		    .eeprom_oe_n(eeprom_oe_n),
-		    .eeprom_cs_n(eeprom_cs_n),
-		    .done(done)
-	      );
+        .reset_n(reset_n),
+        .clock(clock),
+        .address(address),
+        .ram_we_n(ram_we_n),
+        .ram_cs_n(ram_cs_n),
+        .eeprom_oe_n(eeprom_oe_n),
+        .eeprom_cs_n(eeprom_cs_n),
+        .done(done)
+        );
 
-	 initial begin
-		  // Initialize Inputs
-		  reset_n = 0;
-		  clock = 0;
+   initial begin
+      // Initialize Inputs
+      reset_n = 0;
+      clock = 0;
 
-		  // Wait 100 ns for global reset to finish
-		  #100;
+      // Wait 100 ns for global reset to finish
+      #100;
 
-		  // Add stimulus here
+      // Add stimulus here
       reset_n = 1;
    end
 
